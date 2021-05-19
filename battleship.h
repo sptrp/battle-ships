30f0d1eeb9d0576fab6b9b241fc8d2ce2542cce5
Stefan <63808373+stefematik@users.noreply.github.com>
2021-05-19 10:28:35 +0200

    battleship.h upload

---
+++ battleship.h
@@ -0,0 +1,67 @@
+/**
+ * The description of the module.
+ */
+
+#ifndef BATTLESHIP_BATTLESHIP_H
+#define BATTLESHIP_BATTLESHIP_H
+
+#include <boost/shared_ptr.hpp>
+#include <alcommon/almodule.h>
+#include <string>
+
+#include <alproxies/almemoryproxy.h>
+#include <alproxies/albarcodereaderproxy.h>
+#include <alproxies/altexttospeechproxy.h>
+#include <alproxies/alrobotpostureproxy.h>
+#include <althread/almutex.h>
+
+
+namespace AL
+
+{
+  class ALBroker;
+}
+
+class Battleship : public AL::ALModule
+{
+  public:
+
+    Battleship(boost::shared_ptr<AL::ALBroker> broker, const std::string& name);
+
+    virtual ~Battleship();
+
+    /** Overloading ALModule::init().
+    * This is called right after the module has been loaded
+    */
+    virtual void init();
+
+    /**
+    * This method will be called every time the event RightBumperPressed is raised.
+    */
+    void onRightBumperPressed();
+
+    /**
+    * This method will be called every time the event LeftBumperPressed is raised.
+    */
+    void onLeftBumperPressed();
+
+    /**
+    * This method will be called every time a bar code is regonized.
+    */
+    void barcodeRecognized();
+
+  private:
+    AL::ALMemoryProxy fMemoryProxy;
+    AL::ALBarcodeReaderProxy qrCodeProxy;
+    AL::ALTextToSpeechProxy fTtsProxy;
+    AL::ALRobotPostureProxy postureProxy;
+
+    boost::shared_ptr<AL::ALMutex> fCallbackMutex;
+
+    float fState;
+    AL::ALValue qState;
+    std::string field;
+
+};
+
+#endif  // BATTLESHIP_BATTLESHIP_H
