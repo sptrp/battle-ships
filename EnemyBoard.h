f231238628f8d1f160ae4533c7b235415773c147
Stefan <63808373+stefematik@users.noreply.github.com>
2021-06-03 15:50:53 +0200

    newer

--- EnemyBoard.h
+++ EnemyBoard.h
@@ -8,35 +8,37 @@ class EnemyBoard {
 
 private:
     std::vector< std::vector<bool> > board;
-    std::vector< std::vector<int> > blacklist;
+
     std::vector<int> coordinateCache;
     bool rotation;
-    bool shipDestroyed;
 
 
 
-    void ContinueAttacking(int col, int row);
+
+
 
     int AttackColumn(int col, int row, bool downwards);
 
-    int AttackRow(int col, int row, bool forwards);
+    void AttackRow(int col, int row, bool forwards);
 
     bool AttackField();
     void AttackField2();
 
 
 
-    bool IsInBlacklist(int col, int row);
 
-    bool IsWithinGrid(int rowNum, int colNum);
 
 public:
+    bool IsInBlacklist(int col, int row);
+    bool shipDestroyed;
+    bool IsWithinGrid(int rowNum, int colNum);
     bool goForth;
     int shipCounter;
     void StartAttacking(bool continueAttack);
     explicit EnemyBoard(int multiplier);
     void RandomizeCoordinate();
-
+    void ContinueAttacking();
+    std::vector< std::vector<int> > blacklist;
     ~EnemyBoard();
 
 
