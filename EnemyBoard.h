ceb2c0ff88f23e3a62f739262dfc284862b2ae1d
Stefan <63808373+stefematik@users.noreply.github.com>
2021-06-06 17:05:20 +0200

    Update EnemyBoard.h

--- EnemyBoard.h
+++ EnemyBoard.h
@@ -1,4 +1,9 @@
-
+/**
+ @file enemyboard.h
+ @authors Ivan Ponomarev, Stefan Holzbauer
+ @date 17.04.2021
+ @brief Header-File
+*/
 #ifndef BATTLE_SHIPS_ENEMYBOARD_H
 #define BATTLE_SHIPS_ENEMYBOARD_H
 
@@ -7,43 +12,63 @@
 class EnemyBoard {
 
 private:
+    // Variables
+    /**
+     * the board which stores the area for the ships
+     */
     std::vector< std::vector<bool> > board;
 
-    std::vector<int> coordinateCache;
-    bool rotation;
-
-
-
-
-
-
-    int AttackColumn(int col, int row, bool downwards);
-
-    void AttackRow(int col, int row, bool forwards);
-
+    //Methods
+    /**
+     * Try to attack field and set goForth if point need to be saved
+     * @return true if hit, false if missed
+     */
     bool AttackField();
-    void AttackField2();
-
-
-
-
 
 public:
-    bool IsInBlacklist(int col, int row);
+    // Variables
+    /**
+     * bool variable which is true once ship is destroyed
+     */
     bool shipDestroyed;
-    bool IsWithinGrid(int rowNum, int colNum);
-    bool goForth;
-    int shipCounter;
-    void StartAttacking(bool continueAttack);
-    explicit EnemyBoard(int multiplier);
-    void RandomizeCoordinate();
-    void ContinueAttacking();
+    /**
+     * 2 dimensional vector which stores the already attacked or otherwise forbidden attack coordinates
+     */
     std::vector< std::vector<int> > blacklist;
+
+    // Methods
+    /**
+     * Play board constructor
+     * @param multiplier the board will be created with multiplier x multiplier size
+     */
+    explicit EnemyBoard(int multiplier);
+    /**
+     * Create and sets random attack coordinate
+     */
+    void RandomizeCoordinate();
+    /**
+     * Start new attack
+     */
+    void StartAttacking();
+    /**
+     * Check if field is in blacklist because it was shot already
+     * @param col
+     * @param row
+     * @return true if yes, otherwise false
+     */
+    bool IsInBlacklist(int col, int row);
+    /**
+     * Check if field is still in bounds of play board
+     * @param col
+     * @param row
+     * @return true if yes, otherwise false
+     */
+    bool IsWithinGrid(int rowNum, int colNum);
+    /**
+     * Play board destructor
+     */
     ~EnemyBoard();
 
-
-    void AttackWithCoordinates();
-    void PrintBoard();
 };
 
 #endif //BATTLE_SHIPS_ENEMYBOARD_H
